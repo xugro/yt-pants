09e186271bb3dfcc58cb5aad75f72967ace1efe4
xugro <82907135+xugro@users.noreply.github.com>
2024-07-17 19:45:12 +0300

    loop through tabs of youtube channel

--- scriptlet.js
+++ scriptlet.js
@@ -1,5 +1,9 @@
 /// youtube-pants.js
 /// alias yt-pants.js
 (function() {
-  console.log("injection successful");
+	console.log("injection successful");
+	const tabs = document.getElementsByClassName(yt-tab-shape-wiz");
+	for(const element of tabs){
+		console.log(element);
+	}
 })();
