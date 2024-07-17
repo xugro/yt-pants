400b29818c213a660acce2b523ea6ecaaa8fd68c
xugro <82907135+xugro@users.noreply.github.com>
2024-07-17 19:47:27 +0300

    fix class name string

--- scriptlet.js
+++ scriptlet.js
@@ -2,7 +2,7 @@
 /// alias yt-pants.js
 (function() {
 	console.log("injection successful");
-	const tabs = document.getElementsByClassName(yt-tab-shape-wiz");
+	const tabs = document.getElementsByClassName("yt-tab-shape-wiz");
 	for(const element of tabs){
 		console.log(element);
 	}
