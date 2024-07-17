d8884c46279aa004f5b347184b16a5f98576349f
xugro <82907135+xugro@users.noreply.github.com>
2024-07-17 20:48:55 +0300

    apply to all pages if argument isn't set, check the page if argument is set

--- scriptlet.js
+++ scriptlet.js
@@ -6,13 +6,20 @@
 //	console.log("injection successful");
 	(function loop(){
 		setTimeout(() => {
-			var tabs = document.getElementsByClassName("yt-tab-shape-wiz");
-//			console.log("Loaded " + tabs.length + " tabs");
-			for(const element of tabs){
-//				console.log(element);
-				for(const child of element.children){
-					if(child.innerText == "Videos"){
-						child.innerText = "Pants";
+			var flag = false;
+			if(needle != "{{1}}"){
+				const path = window.location.pathname;
+				console.log(path);
+			}
+			if(needle == "{{1}}" || flag == true){
+				var tabs = document.getElementsByClassName("yt-tab-shape-wiz");
+//				console.log("Loaded " + tabs.length + " tabs");
+				for(const element of tabs){
+//					console.log(element);
+					for(const child of element.children){
+						if(child.innerText == "Videos"){
+							child.innerText = "Pants";
+						}
 					}
 				}
 			}
