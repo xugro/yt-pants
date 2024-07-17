58542e851a7ed26fb6a311646c36c150fb59155a
xugro <82907135+xugro@users.noreply.github.com>
2024-07-17 20:23:50 +0300

    limit loop count

--- scriptlet.js
+++ scriptlet.js
@@ -2,15 +2,15 @@
 /// alias yt-pants.js
 (function() {
 	console.log("injection successful");
-	(function loop(){
+	(function loop(count = 0){
 		setTimeout(() => {
 			var tabs = document.getElementsByClassName("yt-tab-shape-wiz");
 			console.log("Loaded " + tabs.length + " tabs");
 			for(const element of tabs){
 				console.log(element);
 			}
-			if(tabs.length == 0){
-				loop();
+			if(tabs.length == 0 && count < 5){ //only attempt 5 times as it loops outside a channel page
+				loop(count + 1);
 			}
 		}, 100);
 	})();
