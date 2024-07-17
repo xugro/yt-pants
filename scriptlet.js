87acbe116eaf31e90e4c7a4e55ea3c4eba6a28af
xugro <82907135+xugro@users.noreply.github.com>
2024-07-17 20:06:07 +0300

    remove continuously reloading as it blocks page from loading

--- scriptlet.js
+++ scriptlet.js
@@ -4,9 +4,6 @@
 	console.log("injection successful");
 	var tabs = document.getElementsByClassName("yt-tab-shape-wiz");
 	console.log("Loaded " + tabs.length + " tabs");
-	while( tabs.length == 0 ){
-		tabs = document.getElementsByClassName("yt-tab-shape-wiz");
-	}
 	for(const element of tabs){
 		console.log(element);
 	}
