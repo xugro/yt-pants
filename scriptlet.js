976f44ae45b3b9902f35d5813199a999eca6925d
xugro <82907135+xugro@users.noreply.github.com>
2024-07-17 19:54:31 +0300

    log how many tabs are loaded

--- scriptlet.js
+++ scriptlet.js
@@ -3,6 +3,7 @@
 (function() {
 	console.log("injection successful");
 	const tabs = document.getElementsByClassName("yt-tab-shape-wiz");
+	console.log("Loaded " + tabs.length + " tabs");
 	for(const element of tabs){
 		console.log(element);
 	}
