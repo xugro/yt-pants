e2831e2c6aa15ba5e822c7164929abcd13398132
xugro <82907135+xugro@users.noreply.github.com>
2024-07-17 20:38:51 +0300

    Removed logs as it keeps spamming

--- scriptlet.js
+++ scriptlet.js
@@ -1,13 +1,13 @@
 /// youtube-pants.js
 /// alias yt-pants.js
 (function() {
-	console.log("injection successful");
+//	console.log("injection successful");
 	(function loop(){
 		setTimeout(() => {
 			var tabs = document.getElementsByClassName("yt-tab-shape-wiz");
-			console.log("Loaded " + tabs.length + " tabs");
+//			console.log("Loaded " + tabs.length + " tabs");
 			for(const element of tabs){
-				console.log(element);
+//				console.log(element);
 				for(const child of element.children){
 					if(child.innerText == "Videos"){
 						child.innerText = "Pants";
